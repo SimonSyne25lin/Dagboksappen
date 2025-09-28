da3e80cb89d78b1b4e76ca7b7e02777406e59e59
Simon <simon.edberg.syne25lin@edu.tucsweden.se>
2025-09-28 16:23:28 +0200

    Skapat skelett och Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
 ﻿namespace Dagboksappen
 {
-    internal class Program
+    public class DiaryEntry
     {
-        static void Main(string[] args)
+        public DateTime Date { get; set; }
+        public string Text { get; set; }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<DiaryEntry> entries = new List<DiaryEntry>
+            {
+                
+            };
+            foreach (var entry in entries)
+            {
+                Console.WriteLine($"{entry.Date.ToShortDateString()}: {entry.Text}");
+            }
         }
     }
 }
