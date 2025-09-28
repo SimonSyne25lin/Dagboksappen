c1fa8f692392b6ebc527583b08cf762d3cf4d7c0
Simon <simon.edberg.syne25lin@edu.tucsweden.se>
2025-09-28 16:37:00 +0200

    Skapade meny och loop för val.

--- Program.cs
+++ Program.cs
@@ -10,14 +10,33 @@
     {
         public static void Main(string[] args)
         {
-            List<DiaryEntry> entries = new List<DiaryEntry>
-            {
+            
+            while (true) {
+                Console.WriteLine("Välj ett alternativ");
+                Console.WriteLine("1. Skriv ny anteckning");
+                Console.WriteLine("2. Anteckningslista");
+                Console.WriteLine("3. Sök anteckningar på datum");
+                Console.WriteLine("4. Spara");
+                Console.WriteLine("5. Läs fil");
+                Console.WriteLine("6. Avsluta");
+                var choice = Console.ReadLine();
                 
-            };
-            foreach (var entry in entries)
-            {
-                Console.WriteLine($"{entry.Date.ToShortDateString()}: {entry.Text}");
-            }
+                switch(choice)
+                {
+                    case "1": AddEntry();
+                        break;
+                    case "2": ListEntries();
+                        break;
+                    case "3": SearchEntry();
+                        break;
+                    case "4": SaveEntry();
+                        break;
+                    case "5": ReadEntry();
+                        break;
+                    case "6": return;
+                    default: Console.WriteLine("Ogiltigt val.");
+                        break;
+                }
         }
     }
 }
